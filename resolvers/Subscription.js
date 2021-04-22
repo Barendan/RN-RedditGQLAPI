f3ab235759247ba3083e531f935b69b89bc2036d
barendan <barendan@gmail.com>
2021-04-22 12:04:05 -0400

    add more subscription functions

--- resolvers/Subscription.js
+++ resolvers/Subscription.js
@@ -3,5 +3,11 @@ const { pubSub } = require("../src/pubSub");
 const postAdded = {
   subscribe: () => pubSub.asyncIterator("postAdded"),
 };
+const postEdited = {
+  subscribe: () => pubSub.asyncIterator("postEdited"),
+};
+const postDeleted = {
+  subscribe: () => pubSub.asyncIterator("postDeleted"),
+};
 
-module.exports = { postAdded };
+module.exports = { postAdded, postEdited, postDeleted };
