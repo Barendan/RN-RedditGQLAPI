6f269955bc1b79e7e44f09af391afa40943e3398
barendan <barendan@gmail.com>
2021-04-22 11:52:04 -0400

    add scripts and fix errors

--- src/pubSub.js
+++ src/pubSub.js
@@ -1,15 +1,15 @@
-const { PostgresPubSub } = require('graphql-postgres-subscriptions');
-const { Client } = require('pg');
+const { PostgresPubSub } = require("graphql-postgres-subscriptions");
+const { Client } = require("pg");
 
-const DATABASE_URL = process.env.DATABASE_URL || 'reddit_api_development';
+const DATABASE_URL = process.env.DATABASE_URL || "reddit_api_development";
 
 const client = new Client({
   connectionString: DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? true : false;
+  ssl: process.env.DATABASE_URL ? true : false,
 });
 
 client.connect();
 
-const pubSub = new PostgresPubSub({ client })
+const pubSub = new PostgresPubSub({ client });
 
 module.exports = { pubSub };
